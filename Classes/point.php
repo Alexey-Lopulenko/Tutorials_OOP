5d7bd0f249c7734aa43181c4fbec9515c6612057
Alexey-Lopulenko <alexlojlua@gmail.com>
2019-04-17 16:41:38 +0300

    lsn1

--- Classes/point.php
+++ Classes/point.php
@@ -1,6 +1,6 @@
 <?php
 
-//namespace Vendor\Model;
+
 class Point
 {
    private $pointA;
@@ -13,7 +13,6 @@ class Point
     {
         $this->pointA = $pointA;
     }
-
     public function getPointA()
     {
         return $this->pointA;
@@ -23,7 +22,6 @@ class Point
     {
         $this->pointB = $pointB;
     }
-
     public function getPointB()
     {
         return $this->pointB;
@@ -33,7 +31,6 @@ class Point
     {
         $this->pointC = $pointC;
     }
-
     public function getPointC()
     {
         return $this->pointC;
@@ -54,12 +51,19 @@ class Point
 
 class Triangle extends Point
 {
+    function __construct()
+    {
+        $this->setPointA([-1,4]);
+        $this->setPointB([-1,2]);
+        $this->setPointC([-7,3]);
+    }
 
     public function square($a , $b, $c)
     {
         $sqaTriangle = 0.5 * (((($a[0] - $c[0]) * ($b[1] - $c[1])) - (($b[0]- $c[0]) * ($a[1]-$c[1]))));
         return $sqaTriangle;
     }
+
     public function perimeter($a,$b,$c)
     {
         $AB = sqrt((($b[0] - $a[0])**2) + (($b[1] - $a[1])**2));
@@ -71,7 +75,76 @@ class Triangle extends Point
     }
 }
 
-//class Rectangle extends Point
-//{
-//
-//}
+
+class Figure extends Point
+{
+    private $square;
+    private $perimeter;
+
+    public function setSquare($square)
+    {
+        $this->square = $square;
+    }
+
+    public function getSquare()
+    {
+        return $this->square;
+    }
+
+    public function setPerimeter($perimeter)
+    {
+        $this->perimeter = $perimeter;
+    }
+
+    public function getPerimeter()
+    {
+        return $this->perimeter;
+    }
+
+
+    function __construct()
+    {
+        $this->setPointA([-4,-2]);
+        $this->setPointB([1,-2]);
+        $this->setPointC([1,-4]);
+        $this->setPointD([-4,-4]);
+
+        $a = $this->getPointA();
+        $b = $this->getPointB();
+        $c = $this->getPointC();
+        $d = $this->getPointD();
+
+
+        if ($a[1] == $b[1] && $a[0] == $d[0] && $d[1] == $c[1] && $b[0] == $c[0]) {
+
+            if (($b[1]-$c[1]) == ($b[0] - $a[0])){
+
+                echo '<br>figure - quadrate';
+
+               $this->setSquare(($b[1]-$c[1])**2);
+               $this->setPerimeter((($b[1]-$c[1])*4));
+            }
+
+            else{
+
+                echo '<br>figure - rectangle';
+
+                $sideA = $b[0] - $a[0];
+                $sideB = $a[1] - $d[1];
+
+                $this->setSquare($sideA * $sideB);
+                $this->setPerimeter(($sideA *2 ) + ($sideB * 2));
+            }
+        }
+
+
+
+
+
+
+
+
+
+
+    }
+}
--- taskProtected.php
+++ taskProtected.php
@@ -3,18 +3,18 @@
 require_once 'Classes/point.php';
 
 
-
-$obPoint = new Point();
-$obPoint->setPointA([-1,4]);
-$obPoint->setPointB([-1,2]);
-$obPoint->setPointC([-7,3]);
-
 $obTriangle = new Triangle();
 
+$SQR = $obTriangle->square($obTriangle->getPointA(),$obTriangle->getPointB(),$obTriangle->getPointC());
+//echo 'Площадь триуголиника = '.$SQR;
 
-$SQR = $obTriangle->square($obPoint->getPointA(),$obPoint->getPointB(),$obPoint->getPointC());
-echo $SQR;
+$perimeter = $obTriangle->perimeter($obTriangle->getPointA(),$obTriangle->getPointB(),$obTriangle->getPointC());
+//echo '<br>Периметр триугольника = '.$perimeter;
+
+$Figure = new Figure();
+$a =$Figure->getSquare();
+$b = $Figure->getPerimeter();
+
+echo '<br>Square - '.$a.'<br>Perimeter - '.$b;
 
 
-$perimeter = $obTriangle->perimeter($obPoint->getPointA(),$obPoint->getPointB(),$obPoint->getPointC());
-echo '<br>'.$perimeter;
